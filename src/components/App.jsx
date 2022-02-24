973375785c3edfb000b0854acb6cfec62d71461f
Julio Labrador <juliotelcel@hotmail.com>
2022-02-24 14:55:10 -0300

    script webpack serve y html index

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = () => {
     return (
-        <h1>Hola eshop</h1>
+        <h1>Hola eshop..</h1>
     );
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
