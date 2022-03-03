34599b8e01287291d8144824dadefe326e2c43e6
Julio Labrador <juliotelcel@hotmail.com>
2022-03-03 17:32:53 -0300

    transformar html y css en componebtes de react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import '../styles/global.scss';
+import Layout from '../containers/Layout';
+import Login from '../containers/Login';
+import '../styles/global.css';
 
 const App = () => {
     return (
-        <h1>Hola eshop..</h1>
+      <Layout>
+        <Login></Login>
+      </Layout>  
     );
 }
 
