b30b9214e459fc2da7b9380b0fa708c468f5a523
Julio Labrador <juliotelcel@hotmail.com>
2022-03-08 15:06:51 -0300

    creacion de componente notFound y header

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import '../styles/global.css';
+
 
 const NotFound = () => {
     return (
-        <div>
-            No encontrado, error 404
+        <div className='container'>
+            <div className='container-center'>
+                    <img src='img/404-error.png'></img>        
+            </div>
         </div>
     );
 }
