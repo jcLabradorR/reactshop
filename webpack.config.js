0b8741b9eaab1d5021a3d2d7d771fbb82ba03a2a
Julio Labrador <juliotelcel@hotmail.com>
2022-03-08 23:55:57 -0300

    imagesnes y alias en webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,13 @@ module.exports = {
     },
     resolve: {
         extensions: ['.js', '.jsx'],
+        alias: {
+           '@components':  path.resolve(__dirname, 'src/components/'),
+           '@containers':  path.resolve(__dirname, 'src/containers/'),
+           '@styles': path.resolve(__dirname, 'src/styles'),
+           '@icons': path.resolve(__dirname, 'src/assets/icons/'),
+           '@logos': path.resolve(__dirname, 'src/assets/logos/'),
+        }
     },
     module: {
         rules: [
@@ -34,8 +41,12 @@ module.exports = {
                 use: [
                     "style-loader",
                     "css-loader",
-                    "sass-loader"
-                    ]
+                    "sass-loader",
+                    ],
+            },
+            {
+               test:  /\.(png|svg|jpg|gif)$/,
+               type: 'asset',
             }
         ]
     },
