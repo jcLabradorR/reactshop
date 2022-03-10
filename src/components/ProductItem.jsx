4b6d0e2247d7d8db12b830f238e64ffa7d86f5a2
Julio Labrador <juliotelcel@hotmail.com>
2022-03-10 00:06:52 -0300

    react useState

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/ProductItem.scss';
+import btAddToCard from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = () => {
+	const[cart, setCard] = useState('');
+
+	const handleClick = () => {
+		setCard('Hola Mundo');
+	}
+
 	return (
 		<div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -10,9 +17,10 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
-					<img src="./icons/bt_add_to_cart.svg" alt="" />
+				<figure onClick={handleClick}>
+					<img src={btAddToCard} alt="" />
 				</figure>
+				{cart}
 			</div>
 		</div>
 	);
