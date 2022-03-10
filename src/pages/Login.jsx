90fb9b7c61ba0d56329ab95a050da5f9c537fc2a
Julio Labrador <juliotelcel@hotmail.com>
2022-03-10 16:33:43 -0300

    formularios y useRef

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../styles/Login.scss';
 
 const Login = () => {
-	return (
+	const form = useRef(null);
+
+	const handleSummit = (event) => {
+		event.preventDefault();
+		const formData = new FormData(form.current);
+		const data = {
+			username: formData.get('email'),
+			password: formData.get('password'),
+		};
+
+		console.log(data);
+	}
+	return(
 		<div className="Login">
 			<div className="Login-container">
 				<img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
-				<form action="/" className="form">
+				<form action="/" className="form" ref={form}>
 					<label htmlFor="email" className="label">Email address</label>
-					<input type="text" id="email" placeholder="platzi@example.cm" className="input input-email" />
+					<input type="text" name="email" placeholder="platzi@example.cm" className="input input-email" />
 					<label htmlFor="password" className="label">Password</label>
-					<input type="password" id="password" placeholder="*********" className="input input-password" />
-					<input type="submit" value="Log in" className="primary-button login-button" />
+					<input type="password" name="password" placeholder="*********" className="input input-password" />
+					<button className="primary-button login-button" onClick={handleSummit}>
+						Log In
+					</button>
 					<a href="/">Forgot my password</a>
 				</form>
 				<button className="secondary-button signup-button">Sign up</button>
