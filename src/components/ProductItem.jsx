3b3ac9f2da17e8c00ae65711c606bff7a886dc3d
Julio Labrador <juliotelcel@hotmail.com>
2022-03-10 15:14:15 -0300

    listado de productos consumiendo una api

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,25 +2,24 @@ import React, { useState } from 'react';
 import '../styles/ProductItem.scss';
 import btAddToCard from '@icons/bt_add_to_cart.svg';
 
-const ProductItem = () => {
+const ProductItem = ({ product }) => {
 	const[cart, setCard] = useState('');
 
 	const handleClick = () => {
-		setCard('Hola Mundo');
+		setCard([]);
 	}
 
 	return (
 		<div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+			<img src={product.images[0]} alt={product.title} />
 			<div className="product-info">
 				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
+					<p>${product.price}</p>
+					<p>{product.title}</p>
 				</div>
 				<figure onClick={handleClick}>
 					<img src={btAddToCard} alt="" />
 				</figure>
-				{cart}
 			</div>
 		</div>
 	);
