823b56769ee030667c0db29a62e862710e8aadcf
Julio Labrador <juliotelcel@hotmail.com>
2022-03-13 21:11:40 -0300

    funcionalidad agregar elementos al carro de compras

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import '@styles/Header.scss';
 import Menu from "@components/Menu";
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
+import AppContext from '../context/AppContext';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
+  const { state } = useContext(AppContext);
 
   const handleToggle = () => {
     setToggle(!toggle);
@@ -46,7 +48,7 @@ const Header = () => {
           <li className="navbar-email" onClick={handleToggle}>platzi@example.com</li>
           <li className="navbar-shopping-cart">
             <img src={shoppingCart} alt="shopping cart" />
-            <div>2</div>
+            { state.cart.length > 0 ? <div>{state.cart.length}</div> : null }
           </li>
         </ul>
       </div>
